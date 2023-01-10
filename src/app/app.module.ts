c63f70256e0a66191b790f6d7227e1c7a9cabf2b
Samuel Emeka <samuelemeka2021@outlook.com>
2023-01-10 14:51:50 +0100

    added owl-carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './guest/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { TrustedCarouselComponent } from './shared/trusted-carousel/trusted-carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    TrustedCarouselComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/trusted-carousel/trusted-carousel.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
+@Component({
+  selector: 'app-trusted-carousel',
+  templateUrl: './trusted-carousel.component.html',
+  styleUrls: ['./trusted-carousel.component.scss']
+})
+export class TrustedCarouselComponent {
+  constructor() { }
+
+  customOptions: OwlOptions = {
+    loop: false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 600,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1 
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    // nav: true,
+    
+  }
+}
+
