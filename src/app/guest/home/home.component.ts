fed1c0910272e866bb8389e40e54d0222865aa02
Samuel Emeka <samuelemeka2021@outlook.com>
2023-01-10 11:20:36 +0100

    added positioning to navbar

--- src/app/guest/home/home.component.ts
+++ src/app/guest/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  // This is the proper positon. don't move the function out of the class! Thank you.
+  @ViewChild ('header') header : ElementRef
+  ngOnInit(): void {
+    window.addEventListener('scroll', () => {
+      const pos = window.scrollY
+      if (pos > 325) {
+        this.header.nativeElement.classList.add('show');
+        this.header.nativeElement.classList.remove('hidden');
+        this.header.nativeElement.style.transition = '2s'
+      }else{
+        this.header.nativeElement.classList.add('hidden');
+        this.header.nativeElement.classList.remove('show');
+        this.header.nativeElement.style.transition = '2s';
+      }
+    })
+  }
 }
+
